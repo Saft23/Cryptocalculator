0dfb5a7447341622ae61a19737ebfe0950f11c5a
Saft23 <joel.ardarve@gmail.com>
2017-12-03 02:31:22 +0100

    Added caches for api

--- calculator.py
+++ calculator.py
@@ -1,13 +1,40 @@
 import requests
+import os
 from pprint import pprint
+import json
+import time
 class Calculator(object):
     def __init__(self):
-        self.adress = "https://api.coinmarketcap.com/v1/ticker/?convert=SEK&limit=0"
-        self.response = requests.get(self.adress)
-        self.data = self.response.json()
-        self.currencyAdress = "https://api.fixer.io/latest?base=USD"
-        self.currencyResponse = requests.get(self.currencyAdress)
-        self.currencyData = self.currencyResponse.json()
+        relativePath = os.path.dirname(__file__)
+        self.coinCachePath = os.path.join(relativePath,'.coincache.json')
+        self.currencyCachePath = os.path.join(relativePath,'.currencycache.json')
+        if not(os.path.isfile(self.coinCachePath)):
+            print("Created coin cache")
+            self.adress = "https://api.coinmarketcap.com/v1/ticker/?limit=0"
+            self.response = requests.get(self.adress)
+            self.data = self.response.json()
+            json.dump(self.data, open('.coincache.json','w'))
+            
+        if not(os.path.isfile(self.currencyCachePath)):
+            print("Created currency cache")
+            self.currencyAdress = "https://api.fixer.io/latest?base=USD"
+            self.currencyResponse = requests.get(self.currencyAdress)
+            self.currencyData = self.currencyResponse.json()
+            json.dump(self.currencyData, open('.currencycache.json','w'))
+
+        with open('.coincache.json') as json_data:
+                self.data = json.load(json_data)
+        with open('.currencycache.json') as json_data:
+                self.currencyData = json.load(json_data)
+
+        currentTime = int(time.time())
+        updateTime = int(self.data[0]['last_updated'])
+        if(updateTime < (currentTime - 600)):
+            updateCache() 
+        
+
+
+
     def priceOfCoin(self, name, currency):
         for item in self.data:
             if item['id'] == name:
@@ -23,6 +50,14 @@ class Calculator(object):
         except Exception: 
             return 0
 
-    def sumOfCoins(self,coinList):
+    def updateCache(self):
+        print("Updated cache")
+        self.adress = "https://api.coinmarketcap.com/v1/ticker/?limit=0"
+        self.response = requests.get(self.adress)
+        self.data = self.response.json()
+        json.dump(self.data, open('.coincache.json','w'))
+        self.currencyAdress = "https://api.fixer.io/latest?base=USD"
+        self.currencyResponse = requests.get(self.currencyAdress)
+        self.currencyData = self.currencyResponse.json()
+        json.dump(self.currencyData, open('.currencycache.json','w'))
         pass
-
